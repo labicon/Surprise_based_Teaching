dab9a72f167629c9387bdc3cbdd0964a4135d003
emmac4 <83370499+emmac4@users.noreply.github.com>
2022-11-07 12:45:39 -0600

    Update cl_policies.py

--- toy_example/cl_policies.py
+++ toy_example/cl_policies.py
@@ -4,17 +4,38 @@ Created on Mon Nov  7 11:37:06 2022
 
 @author: clemm
 """
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
+
 
 class TeacherModel(): 
-    def __init__(self): 
+    def __init__(self, input_dim, output_dim, hidden_layers): 
+        super(TeacherModel, self).__init__()
+        # 1 hidden layer of some dimension -- probably need to change ? 
+        self.layers =nn.Sequential(nn.Linear(input_dim, hidden_layers ), 
+                              nn.Tanh(), 
+                              nn.Linear(hidden_layers,hidden_layers +1  ), 
+                              nn.Tanh())
+        self.actor = nn.Linear(hidden_layers +1, output_dim)
+        self.critic = nn.Linear(hidden_layers +1, 1 )
         return
     
     def forward(self, x): 
-        return 
+        x = self.layers(x)
+        actor = self.actor(x)
+        critic = self.critic(x)
+        
+        return actor, critic 
+    
     
     
 class StudentModel(): 
     def __init__(self): 
+        super(StudentModel, self).__init__()
+        
         return 
     
     def forward(self, x): 
