99b70ead23eaf8d912c8ef0a87924dd11d6f7613
emmac4 <83370499+emmac4@users.noreply.github.com>
2023-03-31 11:05:37 -0400

    Add files via upload
    Cleaned up code a bit. Added separate files that include single agent maximization and student-teacher student surprise maximization sampler functions.

---
+++ toy_example/CurriculumExperiments.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  4 17:55:19 2022
+@author: w044elc
+"""
+from garage.sampler import Sampler, LocalSampler
+from garage.torch.algos import PPO, TRPO, BC
+from garage.torch.policies import GaussianMLPPolicy, DeterministicMLPPolicy, DiscreteQFArgmaxPolicy
+from garage.torch.value_functions import GaussianMLPValueFunction
+from garage.torch.q_functions import DiscreteMLPQFunction
+from garage.trainer import Trainer
+import torch
+from garage import wrap_experiment
+from garage.envs import GymEnv
+from garage.experiment.deterministic import set_seed
+import numpy as np 
+from garage import EpisodeBatch, StepType
+from garage.experiment import deterministic
+from garage.sampler import _apply_env_update
+from garage.sampler.worker import Worker
+import psutil
+from garage.experiment.deterministic import get_seed
+import abc 
+import copy 
+from collections import defaultdict
+from garage import EpisodeBatch
+import SurpriseFunctions
+from SurpriseFunctions import SurpriseWorkerFactory, CustomSampler, SurpriseWorker
+from MaxSurpriseFunctions import MaxSurpriseWorkerFactory, MaxSurpriseWorker
+from StudentOnlyMaxSurpriseFunctions import SOMaxSurpriseWorkerFactory, SOMaxSurpriseWorker
+
+from garage.replay_buffer import ReplayBuffer
+from SparseRewardMountainCar import Continuous_MountainCarEnv
+from setuptools import setup 
+from StudentTeacherAlgo import Curriculum 
+
+
+@wrap_experiment
+def Max_TRPOMC(ctxt=None, seed=1):
+    """Trust Region Policy Optimization 
+    Train a single agent maximizing its own 
+    Sparse reward Mountain Car environment
+    
+    Args:
+        ctxt (garage.experiment.ExperimentContext): The experiment
+            configuration used by Trainer to create the snapshotter.
+        seed (int): Used to seed the random number generator to produce
+            determinism.
+    """
+    set_seed(seed)
+
+    env = GymEnv('Sparse_MountainCar-v0')
+    trainer = Trainer(ctxt)
+    
+    teacher_policy = GaussianMLPPolicy(env.spec,
+                               hidden_sizes=[128, 128],
+                               hidden_nonlinearity=torch.tanh,
+                               output_nonlinearity=None)
+
+    
+    value_function = GaussianMLPValueFunction(env_spec=env.spec,
+                                              hidden_sizes=(128, 128),
+                                              hidden_nonlinearity=torch.tanh,
+                                              output_nonlinearity=None)
+    
+ 
+    
+    surprise = {"surprise": True, "student": None, "eta0": 0.02, "replay": None}
+    
+    teacher_sampler = CustomSampler(envs = env,  
+                           agents = teacher_policy, 
+                           worker_factory = MaxSurpriseWorkerFactory, 
+                           worker_args = surprise, 
+                           max_episode_length = 500)
+    
+    
+
+    
+    
+    algo = TRPO(env_spec = env.spec, 
+               policy=teacher_policy,
+               value_function=value_function,
+               discount=0.995,
+               gae_lambda= .95,
+               center_adv=False, 
+               sampler = teacher_sampler)
+    
+  
+    trainer.setup( algo = algo, env = env)
+    trainer.train(n_epochs = 200, batch_size = 1000)
+    
+    
+@wrap_experiment
+def _BC(ctxt=None, seed=1):
+    """Behaviorial Cloning 
+    Args:
+        ctxt (garage.experiment.ExperimentContext): The experiment
+            configuration used by Trainer to create the snapshotter.
+        seed (int): Used to seed the random number generator to produce
+            determinism.
+    """
+    set_seed(seed)
+    #env = GymEnv('MountainCarContinuous-v0')
+    env = GymEnv('Sparse_MountainCar-v0')
+ 
+    trainer = Trainer(ctxt)
+    #replay_buffer = ReplayBuffer(env_spec = env.spec, size_in_transitions= 10000, time_horizon = 500)
+    
+    
+    student_policy = GaussianMLPPolicy(env.spec,
+                               hidden_sizes=[128, 128],
+                               hidden_nonlinearity=torch.tanh,
+                               output_nonlinearity=None)
+    
+    
+    value_function = GaussianMLPValueFunction(env_spec=env.spec,
+                                              hidden_sizes=(128,128),
+                                              hidden_nonlinearity=torch.tanh,
+                                              output_nonlinearity=None)
+    
+    student_sampler = CustomSampler(envs = env,  
+                           agents = student_policy, 
+                           worker_factory = SurpriseWorkerFactory,  
+                           max_episode_length = 500)
+    
+    surprise = {"surprise": True, "student": student_policy, "eta0": 0.01, "replay": student_sampler}
+    
+    '''
+    
+    from garage.experiment import Snapshotter
+    snapshotter = Snapshotter()
+    data = snapshotter.load('./data/local/experiment/ppo_1')
+    policy = data['algo'].policy
+    '''
+    policy = GaussianMLPPolicy(env.spec,
+                               hidden_sizes=[128, 128],
+                               hidden_nonlinearity=torch.tanh,
+                               output_nonlinearity=None)
+    
+    teacher_sampler = CustomSampler(envs = env,  
+                           agents = policy, 
+                           worker_factory = SurpriseWorkerFactory,
+                           worker_class = SurpriseWorker,
+                           worker_args = surprise, 
+                           max_episode_length = 500)
+ 
+    
+    algo = BC(env_spec = env.spec, 
+              learner = student_policy, 
+              source = policy, 
+              batch_size= 1000, 
+              sampler = teacher_sampler)
+    
+    
+    
+    trainer.setup( algo = algo, env = env)
+    trainer.train(n_epochs = 200, batch_size = 1000)
+    
+    
+@wrap_experiment
+def curriculum_student_teacherMC(ctxt=None, seed=1):
+    """
+    Student-teacher setup for auto-curricula using both teacher and student surprise 
+    Sparse reward Mountain car environment
+    """
+    set_seed(seed)
+    env = GymEnv('Sparse_MountainCar-v0')
+    trainer = Trainer(ctxt)
+    
+    teacher_policy = GaussianMLPPolicy(env.spec,
+                               hidden_sizes=[128, 128],
+                               hidden_nonlinearity=torch.tanh,
+                               output_nonlinearity=None)
+    student_policy = GaussianMLPPolicy(env.spec,
+                               hidden_sizes=[64, 64],
+                               hidden_nonlinearity=torch.tanh,
+                               output_nonlinearity=None)
+    
+    
+    value_function = GaussianMLPValueFunction(env_spec=env.spec,
+                                              hidden_sizes=(128, 128),
+                                              hidden_nonlinearity=torch.tanh,
+                                              output_nonlinearity=None)
+    
+    student_sampler = CustomSampler(envs = env,  
+                           agents = student_policy, 
+                           worker_factory = SurpriseWorkerFactory,  
+                           max_episode_length = 500)
+    
+    surprise = {"surprise": True, "student": student_policy, "eta0": 0.01, "replay": student_sampler}
+    
+    teacher_sampler = CustomSampler(envs = env,  
+                           agents = teacher_policy, 
+                           worker_factory = SurpriseWorkerFactory,
+                           worker_class = SurpriseWorker, 
+                           worker_args = surprise, 
+                           max_episode_length = 500)
+    
+    
+    
+    algo = Curriculum(env_spec = env.spec,
+                      teacher_policy = teacher_policy,
+                      teacher_value_function = value_function, 
+                      student_policy = student_policy, 
+                      teacher_sampler = teacher_sampler,
+                      student_sampler = student_sampler,
+                      batch_size = 1000)
+    
+    trainer.setup( algo = algo, env = env)
+    trainer.train(n_epochs = 200, batch_size = 1000)
+  
+curriculum_student_teacherMC(seed=1)
+
+
+'''
+#run to test & visualize policy 
+from garage.experiment import Snapshotter
+snapshotter = Snapshotter()
+data = snapshotter.load('./data/local/experiment/_TRPO_57')
+policy = data['algo'].policy
+# You can also access other components of the experiment
+env = data['env']
+from garage import rollout
+path = rollout(env, policy, animated=True)
+'''
--- toy_example/StudentTeacherAlgo.py
+++ toy_example/StudentTeacherAlgo.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# yapf: disable
+
 """
 Created on Fri Jan 13 19:34:10 2023
 
@@ -24,53 +26,20 @@ from garage.np import discount_cumsum
 from garage.np.algos import RLAlgorithm
 from garage.torch import compute_advantages, filter_valids
 from garage.torch.optimizers import OptimizerWrapper
+import itertools
 
+from dowel import tabular
+import numpy as np
+import torch
 
-def surp_log_performance(itr, batch, discount, prefix='Evaluation'):
-    """Evaluate the performance of an algorithm on a batch of episodes.
+from garage import (_Default, log_performance, make_optimizer,
+                    obtain_evaluation_episodes, TimeStepBatch)
+from garage.np.algos.rl_algorithm import RLAlgorithm
+from garage.np.policies import Policy
+from garage.sampler import Sampler
+from garage.torch import as_torch
+from garage.experiment import Snapshotter
 
-    Args:
-        itr (int): Iteration number.
-        batch (EpisodeBatch): The episodes to evaluate with.
-        discount (float): Discount value, from algorithm's property.
-        prefix (str): Prefix to add to all logged keys.
-
-    Returns:
-        numpy.ndarray: Undiscounted returns.
-
-    """
-    returns = []
-    undiscounted_returns = []
-    termination = []
-    success = []
-    ext_reward = []
-    for eps in batch.split():
-        returns.append(discount_cumsum(eps.rewards, discount))
-        undiscounted_returns.append(sum(eps.rewards))
-        ext_reward.append(eps.extrinsic_rewards)
-        termination.append(
-            float(
-                any(step_type == StepType.TERMINAL
-                    for step_type in eps.step_types)))
-        if 'success' in eps.env_infos:
-            success.append(float(eps.env_infos['success'].any()))
-
-    average_discounted_return = np.mean([rtn[0] for rtn in returns])
-
-    with tabular.prefix(prefix + '/'):
-        tabular.record('Iteration', itr)
-        tabular.record('NumEpisodes', len(returns))
-        tabular.record('Average Extrinsic Reward', np.mean(ext_reward))
-        tabular.record('AverageDiscountedReturn', average_discounted_return)
-        tabular.record('AverageReturn', np.mean(undiscounted_returns))
-        tabular.record('StdReturn', np.std(undiscounted_returns))
-        tabular.record('MaxReturn', np.max(undiscounted_returns))
-        tabular.record('MinReturn', np.min(undiscounted_returns))
-        tabular.record('TerminationRate', np.mean(termination))
-        if success:
-            tabular.record('SuccessRate', np.mean(success))
-
-    return undiscounted_returns
 class Teacher(VPG):
     """Trust Region Policy Optimization (TRPO).
 
@@ -212,199 +181,9 @@ class Teacher(VPG):
     
     
     """Implementation of Behavioral Cloning in PyTorch."""
-# yapf: disable
-import itertools
 
-from dowel import tabular
-import numpy as np
-import torch
 
-from garage import (_Default, log_performance, make_optimizer,
-                    obtain_evaluation_episodes, TimeStepBatch)
-from garage.np.algos.rl_algorithm import RLAlgorithm
-from garage.np.policies import Policy
-from garage.sampler import Sampler
-from garage.torch import as_torch
-from garage.experiment import Snapshotter
 
-class Student(RLAlgorithm):
-    """Behavioral Cloning.
-
-    Based on Model-Free Imitation Learning with Policy Optimization:
-        https://arxiv.org/abs/1605.08478
-
-    Args:
-        env_spec (EnvSpec): Specification of environment.
-        learner (garage.torch.Policy): Policy to train.
-        batch_size (int): Size of optimization batch.
-        source (Policy or Generator[TimeStepBatch]): Expert to clone. If a
-            policy is passed, will set `.policy` to source and use the trainer
-            to sample from the policy.
-        sampler (garage.sampler.Sampler): Sampler. If source is a policy, a
-            sampler is required for sampling.
-        policy_optimizer (torch.optim.Optimizer): Optimizer to be used to
-            optimize the policy.
-        policy_lr (float): Learning rate of the policy optimizer.
-        loss (str): Which loss function to use. Must be either 'log_prob' or
-            'mse'. If set to 'log_prob' (the default), `learner` must be a
-            `garage.torch.StochasticPolicy`.
-        minibatches_per_epoch (int): Number of minibatches per epoch.
-        name (str): Name to use for logging.
-
-    Raises:
-        ValueError: If learner` is not a
-            `garage.torch.StochasticPolicy` and loss is 'log_prob'.
-
-    """
-
-    # pylint: disable=too-few-public-methods
-
-    def __init__(
-        self,
-        env_spec,
-        learner,
-        *,
-        batch_size,
-        source=None,
-        sampler=None,
-        policy_optimizer=torch.optim.Adam,
-        policy_lr=_Default(1e-3),
-        loss='log_prob',
-        minibatches_per_epoch=16,
-        name='BC',
-    ):
-        self._source = source
-        self.learner = learner
-        self._optimizer = make_optimizer(policy_optimizer,
-                                         module=self.learner,
-                                         lr=policy_lr)
-        if loss not in ('log_prob', 'mse'):
-            raise ValueError('Loss should be either "log_prob" or "mse".')
-        self._loss = loss
-        self._minibatches_per_epoch = minibatches_per_epoch
-        self._eval_env = None
-        self._batch_size = batch_size
-        self._name = name
-
-        # For plotting
-        self.policy = self.learner
-
-        # Public fields for sampling.
-        self._env_spec = env_spec
-        self.exploration_policy = None
-        self.policy = None
-        self.max_episode_length = env_spec.max_episode_length
-        self._sampler = sampler
-        if isinstance(self._source, Policy):
-            self.exploration_policy = self._source
-            self._source = source
-            if not isinstance(self._sampler, Sampler):
-                raise TypeError('Source is a policy. Missing a sampler.')
-        else:
-            self._source = itertools.cycle(iter(source))
-
-    def train(self, trainer):
-        """Obtain samplers and start actual training for each epoch.
-
-        Args:
-            trainer (Trainer): Experiment trainer, for services such as
-                snapshotting and sampler control.
-
-        """
-        if not self._eval_env:
-            self._eval_env = trainer.get_env_copy()
-        for epoch in trainer.step_epochs():
-            if self._eval_env is not None:
-                log_performance(epoch,
-                                obtain_evaluation_episodes(
-                                    self.learner, self._eval_env),
-                                discount=1.0)
-            losses = self._train_once(trainer, epoch)
-            with tabular.prefix(self._name + '/'):
-                tabular.record('MeanLoss', np.mean(losses))
-                tabular.record('StdLoss', np.std(losses))
-
-    def _train_once(self, trainer, epoch):
-        """Obtain samplers and train for one epoch.
-
-        Args:
-            trainer (Trainer): Experiment trainer, which may be used to
-                obtain samples.
-            epoch (int): The current epoch.
-
-        Returns:
-            List[float]: Losses.
-
-        """
-        batch = self._obtain_samples(trainer, epoch)
-        indices = np.random.permutation(len(batch.actions))
-        minibatches = np.array_split(indices, self._minibatches_per_epoch)
-        losses = []
-        for minibatch in minibatches:
-            observations = as_torch(batch.observations[minibatch])
-            actions = as_torch(batch.actions[minibatch])
-            self._optimizer.zero_grad()
-            loss = self._compute_loss(observations, actions)
-            loss.backward()
-            losses.append(loss.item())
-            self._optimizer.step()
-        return losses
-
-    def _obtain_samples(self, trainer, epoch):
-        """Obtain samples from self._source.
-
-        Args:
-            trainer (Trainer): Experiment trainer, which may be used to
-                obtain samples.
-            epoch (int): The current epoch.
-
-        Returns:
-            TimeStepBatch: Batch of samples.
-
-        """
-        if isinstance(self._source, Policy):
-            batch = trainer.obtain_episodes(epoch)
-            log_performance(epoch, batch, 1.0, prefix='Expert')
-            return batch
-        else:
-            batches = []
-            while (sum(len(batch.actions)
-                       for batch in batches) < self._batch_size):
-                batches.append(next(self._source))
-            return TimeStepBatch.concatenate(*batches)
-
-    def _compute_loss(self, observations, expert_actions):
-        """Compute loss of self._learner on the expert_actions.
-
-        Args:
-            observations (torch.Tensor): Observations used to select actions.
-                Has shape :math:`(B, O^*)`, where :math:`B` is the batch
-                dimension and :math:`O^*` are the observation dimensions.
-            expert_actions (torch.Tensor): The actions of the expert.
-                Has shape :math:`(B, A^*)`, where :math:`B` is the batch
-                dimension and :math:`A^*` are the action dimensions.
-
-        Returns:
-            torch.Tensor: The loss through which gradient can be propagated
-                back to the learner. Depends on self._loss.
-
-        """
-        learner_output = self.learner(observations)
-        if self._loss == 'mse':
-            if isinstance(learner_output, torch.Tensor):
-                # We must have a deterministic policy as the learner.
-                learner_actions = learner_output
-            else:
-                # We must have a StochasticPolicy as the learner.
-                action_dist, _ = learner_output
-                learner_actions = action_dist.rsample()
-            return torch.mean((expert_actions - learner_actions)**2)
-        else:
-            assert self._loss == 'log_prob'
-            # We already checked that we have a StochasticPolicy as the learner
-            action_dist, _ = learner_output
-            return -torch.mean(action_dist.log_prob(expert_actions))
-        
 class Curriculum(VPG): 
     def __init__(self, env_spec,
                  teacher_policy,
@@ -418,19 +197,7 @@ class Curriculum(VPG):
                  loss='log_prob',
                  minibatches_per_epoch=16,
                  name='BC'):
-        '''
-        teacher_policy_optimizer=None,
-        vf_optimizer=None,
-        num_train_per_epoch=1,
-        discount=0.99,
-        gae_lambda=0.98,
-        center_adv=True,
-        positive_adv=False,
-        policy_ent_coeff=0.0,
-        use_softplus_entropy=False,
-        stop_entropy_gradient=False,
-        entropy_method='no_entropy'):
-        '''
+    
         
        
         self.teacher_sampler = teacher_sampler
@@ -478,14 +245,12 @@ class Curriculum(VPG):
             self._source = itertools.cycle(iter(self.teacher_policy))
     
     def train(self, trainer):
-        #self.teacher.train(trainer)
+
         last_return = None
         if not self._eval_env:
             self._eval_env = trainer.get_env_copy()
         for _ in trainer.step_epochs():
-            #trainer.policy = self.teacher_policy
             self.policy = self.teacher_policy
-            #trainer._sampler = self.teacher_sampler
             self._sampler = self.teacher_sampler
             for _ in range(self.teacher._n_samples):
                 eps = trainer.obtain_episodes(trainer.step_itr)
@@ -498,11 +263,8 @@ class Curriculum(VPG):
                                     self.teacher_policy, self._eval_env),
                                 discount=1.0, prefix = 'TeacherEval')
                 
-            #self.teacher_policy = trainer.policy 
             self._source = self.teacher_policy 
-            #trainer.policy = self.learner 
             self.policy = self.learner
-            #trainer._sampler = self.student_sampler
             self._sampler = self.student_sampler
             
             if self._eval_env is not None:
@@ -515,33 +277,9 @@ class Curriculum(VPG):
                 tabular.record('MeanLoss', np.mean(losses))
     
                 tabular.record('StdLoss', np.std(losses))
-            #self.learner = trainer.policy 
             self.teacher._sampler.student = self.learner
             
         return last_return
-    
-        
-    
-    def student_train(self, trainer):
-        """Obtain samplers and start actual training for each epoch.
-
-        Args:
-            trainer (Trainer): Experiment trainer, for services such as
-                snapshotting and sampler control.
-
-        """
-        if not self._eval_env:
-            self._eval_env = trainer.get_env_copy()
-        for epoch in trainer.step_epochs():
-            if self._eval_env is not None:
-                log_performance(epoch,
-                                obtain_evaluation_episodes(
-                                    self.learner, self._eval_env),
-                                discount=1.0)
-            losses = self.student_train_once(trainer, epoch)
-            with tabular.prefix(self._name + '/'):
-                tabular.record('MeanLoss', np.mean(losses))
-                tabular.record('StdLoss', np.std(losses))
 
     def student_train_once(self, trainer, epoch):
         """Obtain samplers and train for one epoch.
