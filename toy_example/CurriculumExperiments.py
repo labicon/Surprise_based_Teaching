9b309badb57b4a95e7e6db5a6544a511b1dd4547
emmac4 <83370499+emmac4@users.noreply.github.com>
2023-04-09 18:20:32 -0400

    Update CurriculumExperiments.py
    Made separation between experiments w/ different environments clearer and added specific output directories for logging info for the the different experiments

--- toy_example/CurriculumExperiments.py
+++ toy_example/CurriculumExperiments.py
@@ -6,7 +6,7 @@ Created on Sun Dec  4 17:55:19 2022
 """
 from garage.sampler import Sampler, LocalSampler
 from garage.torch.algos import PPO, TRPO, BC
-from garage.torch.policies import GaussianMLPPolicy, DeterministicMLPPolicy, DiscreteQFArgmaxPolicy
+from garage.torch.policies import GaussianMLPPolicy
 from garage.torch.value_functions import GaussianMLPValueFunction
 from garage.torch.q_functions import DiscreteMLPQFunction
 from garage.trainer import Trainer
@@ -29,14 +29,15 @@ import SurpriseFunctions
 from SurpriseFunctions import SurpriseWorkerFactory, CustomSampler, SurpriseWorker
 from MaxSurpriseFunctions import MaxSurpriseWorkerFactory, MaxCustomSampler, MaxSurpriseWorker
 from StudentOnlyMaxSurpriseFunctions import SOMaxSurpriseWorkerFactory, SOCustomSampler, SOMaxSurpriseWorker
-
+from trpo_discrete import DisTRPO
 from garage.replay_buffer import ReplayBuffer
 from SparseRewardMountainCar import SparseContinuous_MountainCarEnv
 from setuptools import setup 
 from StudentTeacherAlgo import Curriculum 
-
-
-@wrap_experiment
+from DiscreteStudentTeacherAlgo import Dis_Curriculum 
+from DiscretePolicy import DiscreteMLPPolicy
+#Mountain Car
+@wrap_experiment(log_dir = './experiments/MountainCar/MaxTRPO')
 def Max_TRPOMC(ctxt=None, seed=1):
     """Trust Region Policy Optimization 
     Train a single agent maximizing its own 
@@ -158,7 +159,7 @@ def _BC(ctxt=None, seed=1):
     trainer.train(n_epochs = 200, batch_size = 1000)
     
     
-@wrap_experiment
+@wrap_experiment(log_dir = './experiments/MountainCar/Curriculum')
 def curriculum_student_teacherMC(ctxt=None, seed=1):
     """
     Student-teacher setup for auto-curricula using both teacher and student surprise 
@@ -210,14 +211,191 @@ def curriculum_student_teacherMC(ctxt=None, seed=1):
     trainer.setup( algo = algo, env = env)
     trainer.train(n_epochs = 200, batch_size = 1000)
   
-curriculum_student_teacherMC(seed=1)
+#curriculum_student_teacherMC(seed=1)
+
+#####################################################################################
+#Pendulum Env. 
+
+@wrap_experiment(log_dir = './experiments/Pendulum/MaxTRPO')
+def Max_TRPO_P(ctxt=None, seed=1):
+    """Trust Region Policy Optimization 
+    Train a single agent maximizing its own 
+    Pendulum environment
+    
+    Args:
+        ctxt (garage.experiment.ExperimentContext): The experiment
+            configuration used by Trainer to create the snapshotter.
+        seed (int): Used to seed the random number generator to produce
+            determinism.
+    """
+    set_seed(seed)
+
+    env = GymEnv('Pendulum-v1')
+    trainer = Trainer(ctxt)
+    
+    teacher_policy = GaussianMLPPolicy(env.spec,
+                               hidden_sizes=[128, 128],
+                               hidden_nonlinearity=torch.tanh,
+                               output_nonlinearity=None)
+
+    
+    value_function = GaussianMLPValueFunction(env_spec=env.spec,
+                                              hidden_sizes=(128, 128),
+                                              hidden_nonlinearity=torch.tanh,
+                                              output_nonlinearity=None)
+    
+ 
+    
+    surprise = {"surprise": False, "student": None, "eta0": 0.01, "replay": None}
+    
+    teacher_sampler = MaxCustomSampler(envs = env,  
+                           agents = teacher_policy, 
+                           worker_factory = MaxSurpriseWorkerFactory, 
+                           worker_args = surprise, 
+                           max_episode_length = 200)
+    
+    
+
+    
+    
+    algo = TRPO(env_spec = env.spec, 
+               policy=teacher_policy,
+               value_function=value_function,
+               discount=0.995,
+               gae_lambda= .95,
+               center_adv=False, 
+               sampler = teacher_sampler)
+    
+    
+    trainer.setup( algo = algo, env = env)
+    trainer.train(n_epochs = 200, batch_size = 1000)
+
+@wrap_experiment(log_dir = './experiments/Pendulum/curriculum')
+def curriculum_student_teacher_P(ctxt=None, seed=1):
+    """
+    Student-teacher setup for auto-curricula using both teacher and student surprise 
+    Pendulum environment
+    """
+    set_seed(seed)
+    env = GymEnv('Pendulum-v1')
+    trainer = Trainer(ctxt)
+    
+    teacher_policy = GaussianMLPPolicy(env.spec,
+                               hidden_sizes=[128, 128],
+                               hidden_nonlinearity=torch.tanh,
+                               output_nonlinearity=None)
+    
+    student_policy = GaussianMLPPolicy(env.spec,
+                               hidden_sizes=[64, 64],
+                               hidden_nonlinearity=torch.tanh,
+                               output_nonlinearity=None)
+    
+    
+    value_function = GaussianMLPValueFunction(env_spec=env.spec,
+                                              hidden_sizes=(128, 128),
+                                              hidden_nonlinearity=torch.tanh,
+                                              output_nonlinearity=None)
+    
+    student_sampler = CustomSampler(envs = env,  
+                           agents = student_policy, 
+                           worker_factory = SurpriseWorkerFactory,  
+                           max_episode_length = 200)
+    
+    surprise = {"surprise": True, "student": student_policy, "eta0": 0.01, "replay": student_sampler}
+    
+    teacher_sampler = CustomSampler(envs = env,  
+                           agents = teacher_policy, 
+                           worker_factory = SurpriseWorkerFactory,
+                           worker_class = SurpriseWorker, 
+                           worker_args = surprise, 
+                           max_episode_length = 200)
+    
+    
+    
+    algo = Curriculum(env_spec = env.spec,
+                      teacher_policy = teacher_policy,
+                      teacher_value_function = value_function, 
+                      student_policy = student_policy, 
+                      teacher_sampler = teacher_sampler,
+                      student_sampler = student_sampler,
+                      batch_size = 1000)
+    
+    trainer.setup( algo = algo, env = env)
+    trainer.train(n_epochs = 200, batch_size = 1000)
+    
+#curriculum_student_teacher_P(seed = 1)
+#curriculum_student_teacher_P(seed = 2)
+#curriculum_student_teacher_P(seed = 3)
+
+#Max_TRPO_P(seed = 1)
+#Max_TRPO_P(seed = 2)
+#Max_TRPO_P(seed = 3)
 
 
+########################################################################################
+#Cartpole 
+@wrap_experiment(log_dir = './experiments/CartPole/MaxTRPO')
+def Max_TRPO_CP(ctxt=None, seed=1):
+    """Trust Region Policy Optimization 
+    Train a single agent maximizing its own 
+    CartPole environment
+    
+    Args:
+        ctxt (garage.experiment.ExperimentContext): The experiment
+            configuration used by Trainer to create the snapshotter.
+        seed (int): Used to seed the random number generator to produce
+            determinism.
+    """
+    set_seed(seed)
+
+    env = GymEnv('CartPole-v1')
+    trainer = Trainer(ctxt)
+    
+
+    
+    teacher_policy = DiscreteMLPPolicy(env.spec,
+                               hidden_sizes=[128, 128],
+                               hidden_nonlinearity=torch.tanh,
+                               output_nonlinearity=None)
+
+    
+    value_function = GaussianMLPValueFunction(env_spec=env.spec,
+                                              hidden_sizes=(128, 128),
+                                              hidden_nonlinearity=torch.tanh,
+                                              output_nonlinearity=None)
+    
+ 
+    
+    surprise = {"surprise": False, "student": None, "eta0": 0.01, "replay": None}
+    
+    teacher_sampler = MaxCustomSampler(envs = env,  
+                           agents = teacher_policy, 
+                           worker_factory = MaxSurpriseWorkerFactory, 
+                           worker_args = surprise, 
+                           max_episode_length = 500)
+    
+    
+
+    
+    
+    algo = TRPO(env_spec = env.spec, 
+               policy=teacher_policy,
+               value_function=value_function,
+               discount=0.95,
+               gae_lambda= .95,
+               center_adv=False, 
+               sampler = teacher_sampler)
+    
+    
+    trainer.setup( algo = algo, env = env)
+    trainer.train(n_epochs = 1000, batch_size = 1000)
+
+Max_TRPO_CP(seed = 1)
 '''
 #run to test & visualize policy 
 from garage.experiment import Snapshotter
 snapshotter = Snapshotter()
-data = snapshotter.load('./data/local/experiment/_TRPO_57')
+data = snapshotter.load('./experiments/CartPole/MaxTRPO_23')
 policy = data['algo'].policy
 # You can also access other components of the experiment
 env = data['env']
