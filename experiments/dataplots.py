Fix error in surprise calculation

experiments/dataplots.py
@@ -6,18 +6,22 @@ Created on Tue Mar  7 12:11:12 2023
 """
 
 import matplotlib.pyplot as plt 
-import numpy as np
-from numpy import genfromtxt as gen 
-import os
+import numpy as np 
+import argparse
 import pandas as pd
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--env', type=str, default='Sparse_HalfCheetah_Diffspeed')
+parser.add_argument('--experiment_number', type=int, default=1)
 
-env_name = 'Sparse_Hopper_Diffstate'
+args = parser.parse_args()
+
+env_name = args.env
 folder = './' + env_name
 curriculum_folder = folder + '/curriculum'
 MaxTRPO_folder = folder + '/MaxTRPO'
 
-experiment_number = 4
+experiment_number = args.experiment_number
 
 data = []
 for i in range(experiment_number):
@@ -29,7 +33,7 @@ for i in range(experiment_number):
 # student evaluation
 student_evaluation = 'StudentEval/AverageReturn'
 # teacher return with surprise
-teacher_w_surprise = 'TeacherEval/TerminationRate'
+teacher_w_surprise = 'StudentEval/TerminationRate'
 # teacher return without surprise
 teacher_wout_surprise = 'TeacherEval/AverageReturn'
 
@@ -91,29 +95,29 @@ iters = np.linspace(0,epoch,epoch)
 
 
 fig1, ax1 = plt.subplots(3, figsize = (10,10))
-ax1[0].plot(iters, s_mean_c, label = 'ours_eta_0.0001')
+ax1[0].plot(iters, s_mean_c, label = 'ours_eta_0.001')
 ax1[0].fill_between(iters, (s_mean_c + s_std_c), (s_mean_c - s_std_c), alpha = .3)
 
-ax1[0].plot(iters, s_mean_m, label = 'max surp_eta_0.0001.')
+ax1[0].plot(iters, s_mean_m, label = 'max surp_eta_0.001.')
 ax1[0].fill_between(iters, (s_mean_m + s_std_m), (s_mean_m - s_std_m), alpha = .3)
 
 ax1[0].set_title(student_evaluation)
 ax1[0].legend()
 ax1[0].set_xticks([])
 
-ax1[1].plot(iters, t_mean_c, label = 'ours_eta_0.0001')
+ax1[1].plot(iters, t_mean_c, label = 'ours_eta_0.001')
 ax1[1].fill_between(iters, (t_mean_c + t_std_c), (t_mean_c - t_std_c), alpha = .3)
 
-ax1[1].plot(iters, t_mean_m, label = 'max surp_eta_0.0001.')
+ax1[1].plot(iters, t_mean_m, label = 'max surp_eta_0.001.')
 ax1[1].fill_between(iters, (t_mean_m + t_std_m), (t_mean_m - t_std_m), alpha = .3)
 
 ax1[1].set_xticks([])
 ax1[1].legend()
 ax1[1].set_title(teacher_wout_surprise)
 
-ax1[2].plot(iters, ts_mean_c, label = 'ours_eta_0.0001')
+ax1[2].plot(iters, ts_mean_c, label = 'ours_eta_0.001')
 ax1[2].fill_between(iters, (ts_mean_c + ts_std_c), (ts_mean_c - ts_std_c), alpha = .3)
-ax1[2].plot(iters, ts_mean_m, label = 'max surp_eta_0.0001.')
+ax1[2].plot(iters, ts_mean_m, label = 'max surp_eta_0.001.')
 ax1[2].fill_between(iters, (ts_mean_m + ts_std_m), (ts_mean_m - ts_std_m), alpha = .3)
 ax1[2].legend()
 ax1[2].set_title(teacher_w_surprise)

toy_example/MaxSurpriseFunctions.py
@@ -83,9 +83,18 @@ class MaxSurpriseWorker(Worker):
         eta1 = self.eta0 / np.max([1.0, np.mean(np.abs(teacher_reward))])
 
         surprise_reward = -eta1*teacher_log
-        new_reward = torch.tensor(teacher_reward) + surprise_reward.reshape(surprise_reward.shape[0])
+        teacher_surprise = surprise_reward.reshape(surprise_reward.shape[0])
+        new_reward = torch.tensor(teacher_reward) + teacher_surprise
         return new_reward
     
+    def calculate_surprise(self, teacher_reward, student_reward, new_states, states_actions):
+        teacher_log = self.regressor.log_likelihood(states_actions, new_states)
+        eta1 = self.eta0 / np.max([1.0, np.mean(np.abs(teacher_reward))])
+
+        surprise_reward = -eta1*teacher_log
+        teacher_surprise = surprise_reward.reshape(surprise_reward.shape[0])
+        student_surprise = torch.zeros_like(teacher_surprise)
+        return teacher_surprise, student_surprise
     
     def worker_init(self):
         """Initialize a worker."""
@@ -547,3 +556,16 @@ class MaxCustomSampler(Sampler):
         for worker, agent, env in zip(self._workers, self._agents, self._envs):
             worker.update_agent(agent)
             worker.update_env(env)
+
+    def calculate_surprise(self, teacher_returns, student_returns, teacher_new_states, teacher_states_actions):
+        teacher_surprise_list = []
+        student_surprise_list = []
+        for worker in self._workers:
+            teacher_surprise, student_surprise = worker.calculate_surprise(teacher_returns, student_returns, teacher_new_states, teacher_states_actions)
+            teacher_surprise_list.append(teacher_surprise)
+            student_surprise_list.append(student_surprise)
+
+        teacher_surprise = np.concatenate(teacher_surprise_list)
+        student_surprise = np.concatenate(student_surprise_list)
+
+        return np.mean(teacher_surprise), np.mean(student_surprise)

toy_example/StudentTeacherAlgo_Diff.py
@@ -14,8 +14,7 @@ import gym
 from garage.torch.algos import VPG
 from garage.torch.optimizers import (ConjugateGradientOptimizer,
                                      OptimizerWrapper)
-import collections
-import copy
+
 from garage import EpisodeBatch, StepType
 from dowel import tabular
 import numpy as np
@@ -27,7 +26,6 @@ from garage.np.algos import RLAlgorithm
 from garage.torch import compute_advantages, filter_valids
 from garage.torch.optimizers import OptimizerWrapper
 import itertools
-import akro
 
 from dowel import tabular
 import numpy as np
@@ -39,7 +37,8 @@ from garage.np.algos.rl_algorithm import RLAlgorithm
 from garage.np.policies import Policy
 from garage.sampler import Sampler
 from garage.torch import as_torch
-from garage.experiment import Snapshotter
+
+from surprise_log import log_surprise
 
 class Teacher(VPG):
     """Trust Region Policy Optimization (TRPO).
@@ -269,19 +268,23 @@ class Curriculum_Diff(VPG):
             self._sampler = self.student_sampler
             
             if self._eval_env is not None:
-                log_performance(_,
-                                obtain_evaluation_episodes(
+                teacher_evaluation_batch = obtain_evaluation_episodes(
                                     self.teacher_policy, self._eval_env, max_episode_length=self.max_episode_length,
-                                    deterministic = False),
+                                    deterministic = False)
+                log_performance(_,
+                                teacher_evaluation_batch,
                                 discount=1.0, prefix = 'TeacherEval')
                 
             
             if self._student_eval_env is not None:
-                log_performance(_,
-                                obtain_evaluation_episodes(
+                student_evaluation_batch = obtain_evaluation_episodes(
                                     self.learner, self._student_eval_env, max_episode_length=self.max_episode_length,
-                                    deterministic = False),
+                                    deterministic = False)
+                log_performance(_,
+                                student_evaluation_batch,
                                 discount=1.0, prefix='StudentEval')
+                
+            log_surprise(teacher_evaluation_batch, student_evaluation_batch, self.teacher_sampler, discount=1.0, prefix = 'Surprise')
                 
             losses = self.student_train_once(trainer, _)
             with tabular.prefix(self._name + '/'):

toy_example/SurpriseFunctions.py
@@ -77,17 +77,43 @@ class SurpriseWorker(Worker):
         
 
     def SurpriseBonus(self,teacher_reward, student_reward, new_states, states_actions):
-        
-
+        # Teacher surprise wrt the environment
         teacher_log = self.regressor.log_likelihood(states_actions, new_states)
-        student_log = self.student_regressor.log_likelihood(states_actions, new_states)
         eta1 = self.eta0 / np.max([1.0, np.mean(teacher_reward)])
+
+        teacher_surprise = -eta1*teacher_log
+        teacher_surprise = teacher_surprise.reshape(teacher_surprise.shape[0])
+
+        # Student surprise wrt the teacher
         eta2 = self.eta0 / np.max([1.0, np.mean(student_reward)])
-        surprise_reward = -eta1*teacher_log + eta2*(teacher_log - student_log) 
+        # next state sampled from teacher regressor
+        teacher_new_states = self.regressor.sample(states_actions)
+        student_log = self.student_regressor.log_likelihood(states_actions, teacher_new_states)
+        student_surprise = eta2*(teacher_log - student_log)
+        student_surprise = student_surprise.reshape(student_surprise.shape[0])
+
+        surprise_reward = teacher_surprise + student_surprise 
         
         new_reward = torch.tensor(teacher_reward) + surprise_reward.reshape(surprise_reward.shape[0])
         return new_reward
-    
+
+    def calculate_surprise(self, teacher_reward, student_reward, new_states, states_actions):
+        # Teacher surprise wrt the environment
+        teacher_log = self.regressor.log_likelihood(states_actions, new_states)
+        eta1 = self.eta0 / np.max([1.0, np.mean(teacher_reward)])
+
+        teacher_surprise = -eta1*teacher_log
+        teacher_surprise = teacher_surprise.reshape(teacher_surprise.shape[0])
+
+        # Student surprise wrt the teacher
+        eta2 = self.eta0 / np.max([1.0, np.mean(student_reward)])
+        # next state sampled from teacher regressor
+        teacher_new_states = self.regressor.sample(states_actions)
+        student_log = self.student_regressor.log_likelihood(states_actions, teacher_new_states)
+        student_surprise = eta2*(teacher_log - student_log)
+        student_surprise = student_surprise.reshape(student_surprise.shape[0])
+        
+        return teacher_surprise, student_surprise   
     
     def worker_init(self):
         """Initialize a worker."""
@@ -571,3 +597,16 @@ class CustomSampler(Sampler):
         for worker, agent, env in zip(self._workers, self._agents, self._envs):
             worker.update_agent(agent)
             worker.update_env(env)
+
+    def calculate_surprise(self, teacher_returns, student_returns, teacher_new_states, teacher_states_actions):
+        teacher_surprise_list = []
+        student_surprise_list = []
+        for worker in self._workers:
+            teacher_surprise, student_surprise = worker.calculate_surprise(teacher_returns, student_returns, teacher_new_states, teacher_states_actions)
+            teacher_surprise_list.append(teacher_surprise)
+            student_surprise_list.append(student_surprise)
+
+        teacher_surprise = np.concatenate(teacher_surprise_list)
+        student_surprise = np.concatenate(student_surprise_list)
+
+        return teacher_surprise, student_surprise